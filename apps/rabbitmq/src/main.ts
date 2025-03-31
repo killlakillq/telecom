4c0ba3ec9e77eabe06ecf5639a2ee1e8c8f64ad6
killlakillq <draginichdev@gmail.com>
2025-03-31 16:21:35 +0300

    first commit

---
+++ apps/rabbitmq/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'cats_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  await app.listen();
+}
+
+void bootstrap();
